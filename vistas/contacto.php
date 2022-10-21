d95bb1a0af31a4f93d596958d599900c288bc664
= <=>
2022-10-21 11:15:39 -0500

    tuki

--- vistas/contacto.php
+++ vistas/contacto.php
@@ -1,21 +1,22 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pontilaunch</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;700&family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;700&family=Roboto:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../styles/estilos.css">
     <style type="text/css">
-    
-        a{
+        a {
             color: black;
             text-decoration: none;
         }
     </style>
 </head>
+
 <body>
     <div class="head"></div>
     <div class="pontilaunchHome">
@@ -24,24 +25,24 @@
     <div class="pontilaunch1Home">
         <h1>pontilaunch</h1>
     </div>
-    
+
     <!-- Icono casa -->
     <div class="casa">
         <a href="../vistas/home.php">
-        <img src="../imagenes/casa.png" href="../vistas/home.php">
+            <img src="../imagenes/casa.png" href="../vistas/home.php">
         </a>
     </div>
 
     <!-- Icono salir -->
     <div class="salir">
         <a href="../vistas/inicio.php">
-        <img src="../imagenes/cerrar-sesion.png">
+            <img src="../imagenes/cerrar-sesion.png">
         </a>
     </div>
 
     <div class="publicar">
         <form method="post">
-            <input type="text" required placeholder="encabezado" class="publi1" name="encabezado" autocomplete="off">
+            <input type="text" required placeholder="encabezado" class="publi1" name="ePPcabezado" autocomplete="off">
 
             <input type="text" required placeholder="correo de contacto" name="correo" class="publi2" autocomplete="off">
 
@@ -49,9 +50,8 @@
 
             <input type="submit" value="enviar mensaje" name="guardar" class="publi4">
         </form>
-    <?php
-    include('../conexiones/gcontacto.php');
-    ?>
+        <?php
+        include('../conexiones/gcontacto.php');
+        ?>
 
 </html>
-    
--- vistas/home_estudiante.php
+++ vistas/home_estudiante.php
@@ -1,37 +1,38 @@
 <?php
-$conex = mysqli_connect("localhost","root","","pontilaunch");
+$conex = mysqli_connect("localhost", "root", "", "pontilaunch");
 ?>
 
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pontilaunch</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;700&family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;700&family=Roboto:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../styles/estilos.css">
     <style type="text/css">
-    
-        a{
+        a {
             color: black;
             text-decoration: none;
         }
     </style>
 </head>
+
 <body>
     <div class="selecc"></div>
     <!-- Rectangulo amarillo proyectos -->
     <div class="rectanguloUser"></div>
-     <!-- Rectangulo naranja publicar -->
+    <!-- Rectangulo naranja publicar -->
     <a href="../vistas/publicar.php">
         <div class="rectanguloProyect"></div>
-        </a> 
+    </a>
     <!-- Rectangulo rojo grupo -->
     <a href="../vistas/grupo.php">
         <div class="rectanguloGrupo"></div>
-        </a>
+    </a>
     <!-- Icono usuario -->
     <div class="usuarioIcon">
         <img src="../imagenes/usuario.png">
@@ -43,22 +44,22 @@ $conex = mysqli_connect("localhost","root","","pontilaunch");
     <!-- Icono proyecto -->
     <div class="proyectIcon">
         <a href="../vistas/publicar.php">
-        <img src="../imagenes/editar.png">
+            <img src="../imagenes/editar.png">
         </a>
     </div>
     <!-- Texto Proyecto -->
     <div class="proyecText">
         <a href="../vistas/publicar.php">
-        <h1>publicar nuevo proyecto</h1>
+            <h1>publicar nuevo proyecto</h1>
         </a>
     </div>
-  
+
     <div class="grupoText">
         <a href="../vistas/grupo.php">
-        <h1>mi grupo</h1>
+            <h1>mi grupo</h1>
         </a>
     </div>
-    
+
     <div class="head"></div>
     <div class="pontilaunchHome">
         <h1>pontilaunch</h1>
@@ -69,25 +70,25 @@ $conex = mysqli_connect("localhost","root","","pontilaunch");
     <!-- Icono casa -->
     <div class="casa">
         <a href="../vistas/home_estudiante.php">
-        <img src="../imagenes/casa.png" href="../vistas/home_estudiante.php">
+            <img src="../imagenes/casa.png" href="../vistas/home_estudiante.php">
         </a>
     </div>
     <!-- Icono comentario -->
     <div class="comentario">
         <a href="../vistas/mensajes.php">
-        <img src="../imagenes/comentario.png">
+            <img src="../imagenes/comentario.png">
         </a>
     </div>
     <!-- Icono salir -->
     <div class="salir">
         <a href="../vistas/inicio.php">
-        <img src="../imagenes/cerrar-sesion.png">
+            <img src="../imagenes/cerrar-sesion.png">
         </a>
     </div>
     <?php
-    $sql="SELECT * FROM publicaciones";
-    $result=mysqli_query($conex,$sql);
-    while($mostrar=mysqli_fetch_array($result)){
+    $sql = "SELECT * FROM publicaciones";
+    $result = mysqli_query($conex, $sql);
+    while ($mostrar = mysqli_fetch_array($result)) {
     ?>
         <form class="pub" method="post">
             <label class="tit"><?php echo $mostrar['titulo_publicacion'] ?></label>
@@ -96,51 +97,51 @@ $conex = mysqli_connect("localhost","root","","pontilaunch");
             <label class="des"><?php echo $mostrar['descripcion_publicacion'] ?></label>
             <label class="fec"><?php echo $mostrar['fecha_publicacion'] ?></label>
             <button class="boton" name="1star">
-            1
+                1
             </button>
-            <button class="boton"  name="2star">
-            2
+            <button class="boton" name="2star">
+                2
             </button>
-            <button class="boton"  name="3star">
-            3
+            <button class="boton" name="3star">
+                3
             </button>
-            <button class="boton"  name="4star">
-            4
+            <button class="boton" name="4star">
+                4
             </button>
-            <button class="boton"  name="5star">
-            5
+            <button class="boton" name="5star">
+                5
             </button>
-            
+
         </form>
 
     <?php
     }
     ?>
-    <form class="publiOp"method="post">
+    <form class="publiOp" method="post">
         <div class="contact">
             <a href="contacto.php">contactar</a>
-            </div>
-            <div class="califi">califica:</div>
-            <input type="varchar" class ="coment1" placeholder="Agrega tu comentario" name="comentariotxt">
-            <input type="submit" class="coment2" value="comentar" name="coment" >
-            <?php
-            $sqli="SELECT * FROM comentario";
-            $result=mysqli_query($conex,$sqli);
-            while($most=mysqli_fetch_array($result)){
-            ?>
+        </div>
+        <div class="califi">califica:</div>
+        <input type="varchar" class="coment1" placeholder="Agrega tu comentario" name="comentariotxt">
+        <input type="submit" class="coment2" value="comentar" name="coment">
+        <?php
+        $sqli = "SELECT * FROM comentario";
+        $result = mysqli_query($conex, $sqli);
+        while ($most = mysqli_fetch_array($result)) {
+        ?>
             <div class="coments">
-            <h1><?php echo $most['texto_comentario']?></h1>
+                <h1><?php echo $most['texto_comentario'] ?></h1>
             </div>
-                  
+
     </form>
 
 <?php
-}
+        }
 ?>
 <?php
 include('../conexiones/comentarios.php');
 include('../conexiones/calificaciones.php');
 ?>
 </body>
+
 </html>
-    
